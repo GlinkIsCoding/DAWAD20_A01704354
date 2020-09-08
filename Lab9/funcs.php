c60570ac8a84a4b89fb49d464eecc6ebd93d9feb
Guillermo C Espino M <carlos@Guillermos-MacBook-Pro.local>
2020-09-07 23:18:05 -0500

    Lab 9 con funciones menos funcion libre y sin preguntas

--- Lab9/funcs.php
+++ Lab9/funcs.php
@@ -1,4 +1,86 @@
 <?php
+
+function promedio($array){
+    $size = count($array);
+    $listanums = "<ul>";
+    $suma = 0;
+    $listanums .= "<h5>Lista de números recibidos</h5>";
+    for($i = 0; $i<$size;$i++){
+        $listanums .= "<li>$array[$i]</li>";
+        $suma = $suma + $array[$i];
+    }
+    $promedio = $suma/$size;
+    $listanums .= "<h5>Promedio</h5>";
+    $listanums .= "<li>$promedio</li>";
+    $listanums .= "</ul>";
+    return $listanums;
+}
+
+function mediana($array){
+    $size = count($array);
+    $listanums = "<ul>";
+    $listanums .= "<h5>Lista de números recibidos</h5>";
+    for($i = 0; $i<$size;$i++){
+        $listanums .= "<li>$array[$i]</li>";
+    }
+    
+    sort($array);
+    $mediana;
+    $pos = $size/2;
+    if($size%2 == 0){
+        
+        $mediana = ($array[$pos] + $array[$pos -1])/2;
+    }
+    else{
+        $pos = floor($pos);
+        $mediana = $array[$pos];
+    }
+    $listanums .= "<h5>Mediana</h5>";
+    $listanums .= "<li>$mediana</li>";
+    $listanums .= "</ul>";
+    return $listanums;
+}
+
+function arreglolistas($array){
+    $size = count($array);
+    $listanums = "<ul>";
+    $suma = 0;
+    $listanums .= "<h5>Lista de números recibidos</h5>";
+    for($i = 0; $i<$size;$i++){
+        $listanums .= "<li>$array[$i]</li>";
+        $suma = $suma + $array[$i];
+    }
+    
+    sort($array);
+    $promedio = $suma/$size;
+    $listanums .= "<h5>Promedio</h5>";
+    $listanums .= "<li>$promedio</li>";
+    $mediana;
+    $pos = $size/2;
+    if($size%2 == 0){
+        
+        $mediana = ($array[$pos] + $array[$pos -1])/2;
+    }
+    else{
+        $pos = floor($pos);
+        $mediana = $array[$pos];
+    }
+    $listanums .= "<h5>Mediana</h5>";
+    $listanums .= "<li>$mediana</li>";
+    
+    $listanums .= "<h5>Lista de números orden ascendente</h5>";
+   for($i = 0; $i<$size;$i++){
+        $listanums .= "<li>$array[$i]</li>";
+    }
+    rsort($array);
+    $listanums .= "<h5>Lista de números orden descendente</h5>";
+   for($i = 0; $i<$size;$i++){
+        $listanums .= "<li>$array[$i]</li>";
+    }
+    
+    $listanums .= "</ul>";
+    return $listanums;
+}
     
 function tablacuadradocubo($x){
         $tabla = "<table>";
@@ -26,4 +108,5 @@ function tablacuadradocubo($x){
         $tabla .= "</table>";
         return $tabla;
     }
+
 ?>
--- Lab9/index.php
+++ Lab9/index.php
@@ -5,7 +5,36 @@ include("_header.html");
 
 <h1 class="center-align">Lab 9</h1>
 
+<h3 class="center-align">Promedio de arrays</h3>
 
+<div class="container centered">
+<?php
+$grupo1 = array(1, 2, 3, 4, 5);
+$grupo2 = array(40,60,70,80,90);
+    echo promedio($grupo1);
+    echo promedio($grupo2);
+?>
+</div>
+
+<h3 class="center-align">Mediana de arrays</h3>
+
+<div class="container centered">
+<?php
+    echo mediana($grupo1);
+    echo mediana(array(70,40,60,80,90));
+?>
+</div>
+
+<h3 class="center-align">Listas de números</h3>
+
+<div class="container centered">
+<?php
+$lista1 = array(90, 4, 6, 7, 8, 2);
+$lista2 = array(56, 78, 90, 2, 1, 3);
+echo arreglolistas($lista1);
+echo arreglolistas($lista2);
+?>
+</div>
 
 
 
