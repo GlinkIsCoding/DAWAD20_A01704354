2a47c5723f0b897dc189638fae5f46c6be37eff8
Guillermo C Espino M <carlos@Guillermos-MacBook-Pro.local>
2020-09-25 14:24:31 -0500

    Corregir path de subida de imagen con localhost, en vez de VM

--- Lab12/upload.php
+++ Lab12/upload.php
@@ -9,7 +9,7 @@ clear_entries();
 
 include("_header.html");
 
-$target_dir = "/opt/lampp/htdocs/Lab12/uploads/";
+$target_dir = "/Applications/XAMPP/xamppfiles/htdocs/Lab12/uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -52,7 +52,7 @@ if ($uploadOk == 0) {
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    $_SESSION["newimage"] = "http://192.168.64.2/Lab12/uploads/".$_FILES["fileToUpload"]["name"] ;
+    $_SESSION["newimage"] = "uploads/".$_FILES["fileToUpload"]["name"] ;
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
